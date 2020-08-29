5945eba4ab8c9f304077c28f385d27390c0cf334
azie-ginanjar <xstepnort@gmail.com>
2020-08-29 09:43:21 +0700

    add method to check if number is possible to put on specified location, and add its test

--- solution/solution.py
+++ solution/solution.py
@@ -15,6 +15,30 @@ def retrieve_grid():
     return sudoku_grid
 
 
+def is_possible(row, col, n, grid):
+    # validate that n not appeared yet in rows
+    if n in grid[row]:
+        return False
+
+    # validate that n not appeared yet in cols
+    for i in range(0, 9):
+        if grid[i][col] == n:
+            return False
+
+    # validate that n not appeared yet on box
+
+    # define start coordinate of box in 9x9 grid
+    box_row = (row // 3) * 3
+    box_col = (col // 3) * 3
+
+    for i in range(3):
+        for j in range(3):
+            if grid[box_row+i][box_col+j] == n:
+                return False
+
+    return True
+
+
 if __name__ == '__main__':
     grid = retrieve_grid()
     print(grid)
--- solution/tests/test_solution.py
+++ solution/tests/test_solution.py
@@ -1,6 +1,6 @@
 import pytest
 
-from ..solution import split_into_int, retrieve_grid
+from ..solution import split_into_int, retrieve_grid, is_possible
 
 
 def test_split_into_int():
@@ -19,3 +19,27 @@ def test_retrieve_grid():
     grid = retrieve_grid()
 
     assert grid[0][0] == [0, 0, 3, 0, 2, 0, 6, 0, 0]
+
+
+def test_is_possible():
+    grid = [[0, 0, 3, 0, 2, 0, 6, 0, 0],
+            [9, 0, 0, 3, 0, 5, 0, 0, 1],
+            [0, 0, 1, 8, 0, 6, 4, 0, 0],
+            [0, 0, 8, 1, 0, 2, 9, 0, 0],
+            [7, 0, 0, 0, 0, 0, 0, 0, 8],
+            [0, 0, 6, 7, 0, 8, 2, 0, 0],
+            [0, 0, 2, 6, 0, 9, 5, 0, 0],
+            [8, 0, 0, 2, 0, 3, 0, 0, 9],
+            [0, 0, 5, 0, 1, 0, 3, 0, 0]]
+
+    # validate this return false if number already in 3x3 box
+    assert not is_possible(0, 0, 1, grid)
+
+    # validate this return false if number already in row
+    assert not is_possible(0, 0, 3, grid)
+
+    # validate this return false if number already in col
+    assert not is_possible(0, 0, 7, grid)
+
+    # validate this return true if number not in col, row, or 3x3 box
+    assert is_possible(0, 0, 4, grid)
